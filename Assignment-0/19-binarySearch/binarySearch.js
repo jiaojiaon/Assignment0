d8956d1e866fec9fd496292979da2c206738016f
jiaojiaon <90946653+jiaojiaon@users.noreply.github.com>
2021-12-27 20:37:15 -0500

    Update binarySearch.js

--- Assignment-0/19-binarySearch/binarySearch.js
+++ Assignment-0/19-binarySearch/binarySearch.js
@@ -4,7 +4,18 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+    var start = 0;
+    var end = nums.length-1;
+    while(start <= end){
+      var mid = Math.floor((start + end)/2);
+      if(arr[mid] == target) return true;
+      else if(arr[mid] < target){
+        start = mid+1;
+      }
+      else
+        end = mid-1;
+    }
+    return false;
   }
 }
 
