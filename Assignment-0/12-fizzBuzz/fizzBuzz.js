cd80802273a431359aaa5b2141a142a04f0a6769
jiaojiaon <90946653+jiaojiaon@users.noreply.github.com>
2022-01-01 21:41:33 -0500

    Update fizzBuzz.js

--- Assignment-0/12-fizzBuzz/fizzBuzz.js
+++ Assignment-0/12-fizzBuzz/fizzBuzz.js
@@ -1,22 +1,19 @@
 function fizzBuzz(start, end) {
   var arr = new Array();
-    arr[0] = start;
-    for(var i = 0; i < end; i++){
-      arr[i+1] = arr[i]+1;
-    }
-    for(var j = 1; j<= arr; i++){
+    for(var j = start; j<= end; j++){
       if(j%3 == 0 && j%5 == 0){
-        arr[j-1] = "FizzBuzz";
-      }else if(i%3 == 0){
-        arr[j-1] = "Fizz";
-      }else if(i%5 == 0){
-        arr[j-1] == "Buzz";
+        arr.push("FizzBuzz");
+      }else if(j%3 == 0){
+        arr.push("Fizz");
+      }else if(j%5 == 0){
+        arr.push("Buzz");
       }
       else{
-        arr[j-1] =j;
+        arr.push(j);
       }
     }
     return arr;
+  }
 }
 
 // Do not edit this line;
