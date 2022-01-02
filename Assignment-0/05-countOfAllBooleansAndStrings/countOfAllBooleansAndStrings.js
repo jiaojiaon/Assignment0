fd1902eb09c1bf7ec892f1c75738e2666c469d62
jiaojiaon <90946653+jiaojiaon@users.noreply.github.com>
2022-01-01 21:37:49 -0500

    Update countOfAllBooleansAndStrings.js

--- Assignment-0/05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ Assignment-0/05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,7 +1,11 @@
 function countOfAllBooleansAndStrings(arr) {
-  var countB = arr.filter(Boolean).length;
-    var countS = arr.filter(arr => arr).length;
-    return countB + countS;
+  var count = 0;
+      for(var i = 0; i<arr.length; i++){
+        if(typeof arr[i] === 'string' || typeof arr[i] === 'boolean'){
+          count++;
+        }
+      }
+      return count;
 }
 
 // Do not edit this line;
