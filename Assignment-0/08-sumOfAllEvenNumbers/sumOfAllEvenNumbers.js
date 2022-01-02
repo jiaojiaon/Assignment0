14422e6a87175a029d6081711cbc4b44671e80c8
jiaojiaon <90946653+jiaojiaon@users.noreply.github.com>
2022-01-01 21:39:37 -0500

    Update sumOfAllEvenNumbers.js

--- Assignment-0/08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ Assignment-0/08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,6 @@
 function sumOfAllEvenNumbers(nums) {
   var count = 0;
-    for(var i = 0; i< nums.length(); i++){
+    for(var i = 0; i< nums.length; i++){
       if(nums[i]%2 == 0){
         count++;
       }
